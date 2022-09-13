8e22b79ce7ad16429843688a7ae6405987dec26a
RHainz <rogerhainz@gmail.com>
2022-09-13 10:38:26 -0300

    Limpo - Pronto para Consult

--- src/components/FormPericias/index.js
+++ src/components/FormPericias/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-export function FormDescritivo() {
+export function FormPericias() {
     
     const [formDes, setForm] = useState ({
         desNome:"",
--- src/pages/BuildPage/index.js
+++ src/pages/BuildPage/index.js
@@ -1,32 +1,14 @@
 import { FormPrimario } from "../../components/FormPrimario"
-import { FormDescritivo } from "../../components/FormDescritivo"
+import { FormPericias} from "../../components/FormPericias"
 
 export function BuildPage() {
     return (<>
             <div className="dadosDescritivo">
-                <FormDescritivo />
-            </div>
-            <div className="dadosPrimario">
                 <FormPrimario />
             </div>
-            <div className="dadosPericias">
-                <h3>Forçar x0  </h3>
-                <p> Empurrarx0 </p>
+            <div className="dadosPrimario">
+                <FormPericias />
             </div>
-            <div className="dadosTrunfos">
-                <h1>Trunfos</h1>
-                <h2>Sinas</h2>
-            </div>
-            <div className="dadosSecundarios">
-                <h1>Vitalidade</h1>
-                <h2>Dano Básico</h2>
-            </div>
-            <div className="dadosInventario">
-                <h1>Slot Básico</h1>
-                <h2>1</h2>
-            </div>
-            </>
+            </>  
         )
-
-
 }
