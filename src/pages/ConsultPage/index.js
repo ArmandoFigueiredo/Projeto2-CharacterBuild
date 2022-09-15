b66ee9e66cb534fbdb8be396601fcd5208e97eee
ArmandoFigueiredo <armandopfigueiredo@gmail.com>
2022-09-15 17:20:48 -0300

    editar e delete

--- src/pages/ConsultPage/index.js
+++ src/pages/ConsultPage/index.js
@@ -7,12 +7,12 @@ import {useState} from "react"
 export function ConsultPage () {
 
     const [currFichaId, setCurrFichaId] = useState ("6321e0ae4bf6cd00178ada5e");
-    const [editOk, setEditOk]=useState(true);
+    const [editOk, setEditOk]=useState(false);
     
     return (<> 
     <div className="d-flex">
     {!editOk ? <CharacterFace setCurrFichaId = {setCurrFichaId}/>:<></>}
-    {!editOk ? <CharacterDetail currFichaId = {currFichaId}/>:<></>}
+    {!editOk ? <CharacterDetail currFichaId = {currFichaId} setEditOk = {setEditOk}/>:<></>}
     {editOk ? <EditPage currFichaId={currFichaId}/> : <></>}
     </div>
 
